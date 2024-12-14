24cb596bb6fab669a2919681efb267c0e3c973d8
Mehdi71x1 <152052947+Mehdi71x1@users.noreply.github.com>
2024-12-14 23:59:31 +0600

    Add files via upload

---
+++ dragon-ddos.py
@@ -0,0 +1,8 @@
+from art import text2art
+
+# বড় অক্ষরে বাংলা টেক্সট প্রদর্শন
+text = "ডিডস খুজা বাদ দিয়ে এখন ঘুমাও"
+
+# টেক্সটকে ASCII আর্ট আকারে দেখানো
+ascii_art = text2art(text)
+print(ascii_art)
